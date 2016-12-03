6d4ee55fa80937a351da7f31ab36555602400e17
Hasuni <zarembo.kat@yandex.ru>
2016-12-03 10:31:44 +0500

    Update models.py

--- pyramid_blogr/models.py
+++ pyramid_blogr/models.py
@@ -30,19 +30,24 @@ class User(BaseObject):
     name = Column(Unicode(255), unique=True, nullable=False)
     password = Column(Unicode(255), nullable=False)
     last_logged = Column(DateTime, default=datetime.datetime.utcnow)
+    child = relationship("user_status", uselist=False, back_populates="parent")
+    child = relationship("art_user", uselist=False, back_populates="parent")
 
 #статусы
 class Status(Base):
     __tablename__ = 'status'
     id_S = Column(Integer, primary_key=True)
     name = Column(Unicode(255), nullable=False, unique=True)
+    child = relationship("user_status", uselist=False, back_populates="parent")
 
 #соединим Пользоваталей и Статусы
 class UserStatus(Base):
     __tablename__ = 'user_status'
     sv_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey=('users.id_U'))
-    status_id=Column(Integer, ForeignKey=('status.id_S'))
+    user_id = Column(Integer, ForeignKey=('parent1.id_U'))
+    parent1 = relationship("users", back_populates="child")
+    status_id=Column(Integer, ForeignKey=('parent2.id_S'))
+    parent2 = relationship("status", back_populates="child")
 
 #статьи
 class Article(BaseObject):
@@ -52,9 +57,12 @@ class Article(BaseObject):
     content = Column(UnicodeText, default=u'')
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
+    child = relationship("art_user", uselist=False, back_populates="parent")
 
     #соединим Статьи и их авторов:
 class UserArticle(BaseObject):
     __tablename__ = 'art_user'
-    id = Column(Integer, ForeignKey=('articles.id_A'))
-    user_id = Column(Integer, ForeignKey=('users.id_U'))
+    id = Column(Integer, ForeignKey=('parent3.id_A'))
+    parent3 = relationship("articles", back_populates="child")
+    user_id = Column(Integer, ForeignKey=('parent4.id_U'))
+    parent4 = relationship("users", back_populates="child")
