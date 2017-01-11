2b2c466919ebc00f0e8530c55d22f4b65942e934
Hasuni <hasuni@localhost.localdomain>
2017-01-11 16:05:25 +0500

    изменено:      setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,19 @@ with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
 requires = [
-    'pyramid',
+   'pyramid',
     'pyramid_jinja2',
-    'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'pyramid_tm',
-    'SQLAlchemy',
+    'SQLAlchemy>=1.0',
     'transaction',
     'zope.sqlalchemy',
-    'waitress'
+    'waitress',
+    'wtforms==2.1',  # form library
+    'webhelpers2==2.0', # various web building related helpers
+    'paginate==0.5.4', # pagination helpers
+    'paginate_sqlalchemy==0.2.0',
+    'passlib'
     ]
 
 setup(name='pyramid_blogr',
