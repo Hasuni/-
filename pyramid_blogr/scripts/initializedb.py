3efecd8f3902a920f279a4c05a8fffecde29ccf2
Hasuni <hasuni@localhost.localdomain>
2017-01-12 19:38:27 +0500

    новый файл:    pyramid_blogr.sqlite 	изменено:      pyramid_blogr/__init__.pyc 	изменено:      pyramid_blogr/models.pyc 	изменено:      pyramid_blogr/scripts/__init__.pyc 	изменено:      pyramid_blogr/scripts/initializedb.py 	изменено:      pyramid_blogr/scripts/initializedb.pyc

--- pyramid_blogr/scripts/initializedb.py
+++ pyramid_blogr/scripts/initializedb.py
@@ -9,12 +9,23 @@ from pyramid.paster import (
 from pyramid.scripts.common import parse_vars
 from ..models import DBSession, Base, User, Article 
 
+def usage(argv):
+    cmd = os.path.basename(argv[0])
+    print('usage: %s <config_uri> [var=value]\n'
+          '(example" @%s development.ini")' % (cmd, cmd))
+    sys.exit(1)
 
 def main(argv=sys.argv):
-    DBSession = Sessipn(bind=engine)
-    Base.metadata.drop_all(engine)
-    Base.metadata.create_all(engine)
+    if len(argv)<2:
+        usage(argv)
+    config_uri = argv[1]
+    options = parse_vars(argv[2:])
+    setup_logging(config_uri)
+    settings = get_appsettings(config_uri, options=options)
 
-    user = User(name="admin",password="123456")
-    DBSession.add(user)
-    DBSession.commit()
+    engine = engine_from_config(settings, 'sqlalchemy.')
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    #with transaction.manager:
+    #    model = MyModel(name='one',value=1)
+    #    DBSession.add(model)
